9e152afbd2e87d514f2b24cf49e60a3575fc544a
유재건 <jamy78@naver.com>
2023-03-24 20:36:47 +0900

    main

--- src/main/java/com/controller/FirstServlet.java
+++ src/main/java/com/controller/FirstServlet.java
@@ -7,9 +7,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class FirstServlet
- */
 @WebServlet("/FirstServlet")
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
