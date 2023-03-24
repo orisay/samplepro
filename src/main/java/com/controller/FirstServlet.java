74fd6b93afbe5e487e3b01e56a142a2aa7028c5a
유재건 <jamy78@naver.com>
2023-03-24 20:39:57 +0900

    main

--- src/main/java/com/controller/FirstServlet.java
+++ src/main/java/com/controller/FirstServlet.java
@@ -21,6 +21,7 @@ public class FirstServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("메인업데이트");
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
